e93ea5480ae465b3a47771afa76265a83c6f27c4
Peter Farrow <pnfarrow@gmail.com>
2024-06-07 12:43:53 -0600

    almost done

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-import express from 'express';
-import nunjucks from 'nunjucks';
-import morgan from 'morgan';
-import session from 'express-session';
-import users from './users.json' assert { type: 'json' };
-import stuffedAnimalData from './stuffed-animal-data.json' assert { type: 'json' };
+import express from "express";
+import nunjucks from "nunjucks";
+import morgan from "morgan";
+import session from "express-session";
+import users from "./users.json" assert { type: "json" };
+import stuffedAnimalData from "./stuffed-animal-data.json" assert { type: "json" };
 
 const app = express();
-const port = '8000';
+const port = "8000";
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static('public'));
-app.use(session({ secret: 'ssshhhhh', saveUninitialized: true, resave: false }));
+app.use(express.static("public"));
+app.use(
+  session({ secret: "ssshhhhh", saveUninitialized: true, resave: false })
+);
 
-nunjucks.configure('views', {
+nunjucks.configure("views", {
   autoescape: true,
   express: app,
 });
@@ -22,64 +24,74 @@ function getAnimalDetails(animalId) {
   return stuffedAnimalData[animalId];
 }
 
-app.get('/', (req, res) => {
-  res.render('index.html');
+app.get("/", (req, res) => {
+  res.render("index.html");
 });
 
-app.get('/all-animals', (req, res) => {
-  res.render('all-animals.html.njk', { animals: Object.values(stuffedAnimalData) });
+app.get("/all-animals", (req, res) => {
+  res.render("all-animals.html.njk", {
+    animals: Object.values(stuffedAnimalData),
+  });
 });
 
-app.get('/animal-details/:animalId', (req, res) => {
-  res.render('animal-details.html.njk', { animal: stuffedAnimalData.elephant });
+app.get("/animal-details/:animalId", (req, res) => {
+  const animalDetails = getAnimalDetails(req.params.animalId);
+  res.render("animal-details.html.njk", { animal: animalDetails });
 });
 
-app.get('/add-to-cart/:animalId', (req, res) => {
-  // TODO: Finish add to cart functionality
-  // The logic here should be something like:
-  // - check if a "cart" exists in the session, and create one (an empty
-  // object keyed to the string "cart") if not
-  // - check if the desired animal id is in the cart, and if not, put it in
-  // - increment the count for that animal id by 1
-  // - redirect the user to the cart page
+app.get("/add-to-cart/:animalId", (req, res) => {
+  const sesh = req.session;
+  const animalId = req.params.animalId;
+
+  if (!sesh.cart) {
+    sesh.cart = {};
+  }
+  if (!sesh.cart[animalId]) {
+    sesh.cart[animalId] = 0;
+  }
+  sesh.cart[animalId] += 1;
+
+  console.log(sesh.cart);
+  res.redirect("/cart");
 });
 
-app.get('/cart', (req, res) => {
-  // TODO: Display the contents of the shopping cart.
+app.get("/cart", (req, res) => {
+  const sesh = req.session;
+  const cart = sesh.cart;
 
-  // The logic here will be something like:
+  const animalsInCart = [];
+  let total = 0;
 
-  // - get the cart object from the session
-  // - create an array to hold the animals in the cart, and a variable to hold the total
-  // cost of the order
-  // - loop over the cart object, and for each animal id:
-  //   - get the animal object by calling getAnimalDetails
-  //   - compute the total cost for that type of animal
-  //   - add this to the order total
-  //   - add quantity and total cost as properties on the animal object
-  //   - add the animal object to the array created above
-  // - pass the total order cost and the array of animal objects to the template
+  for (const animal in cart) {
+    const animalObj = getAnimalDetails(animal);
+    animalObj["quantity"] = cart[`${animal}`];
+    const subtotal = animalObj["quantity"] * animalObj["price"];
+    animalObj["subtotal"] = subtotal;
+    total += subtotal;
+    console.log(total);
+    animalsInCart.push(animalObj);
+  }
 
   // Make sure your function can also handle the case where no cart has
   // been added to the session
 
-  res.render('cart.html.njk');
+  res.render("cart.html.njk", { animalsInCart: animalsInCart, total: total });
 });
 
-app.get('/checkout', (req, res) => {
+app.get("/checkout", (req, res) => {
   // Empty the cart.
   req.session.cart = {};
-  res.redirect('/all-animals');
+  res.redirect("/all-animals");
 });
 
-app.get('/login', (req, res) => {
+app.get("/login", (req, res) => {
   // TODO: Implement this
-  res.send('Login has not been implemented yet!');
+  res.send("Login has not been implemented yet!");
 });
 
-app.post('/process-login', (req, res) => {
+app.post("/process-login", (req, res) => {
   // TODO: Implement this
-  res.send('Login has not been implemented yet!');
+  res.send("Login has not been implemented yet!");
 });
 
 app.listen(port, () => {
